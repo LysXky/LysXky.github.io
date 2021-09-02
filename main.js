6d13f210cdd9a6e969773c0f8ba0b2b0e2feb52d
LysXky <felipetapiaparra23@gmail.com>
2021-09-01 21:48:22 -0400

    Mejoras

---
+++ main.js
@@ -0,0 +1,10 @@
+<script>
+    window.onload = function() {
+        var contenedor = document.getElementByClass('cont_load');
+
+        contenedor.style.visibility = 'hidden';
+        contenedor.style.opacity = '0';
+    }
+
+
+</script>
